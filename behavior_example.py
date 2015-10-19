f4b3c0366fb502303a2022a954a532c80f733dd4
stianste <stiansteinbakken17@gmail.com>
2015-10-19 18:38:43 +0200

    Added an example of how the Behavior class can be used (wihtout sensory)

---
+++ behavior_example.py
@@ -0,0 +1,36 @@
+from Behavior import *
+
+class b_hunger(Behavior):
+    
+    def __init__(self, bbcon):
+        #Init hunger with the bbcon it takes in, no sensors, not active and with a high static priority = 10
+        super(self, bbcon, None, False, 10).__init__()
+        #Introduce a hunger level. The higher the hunger level, the more satisfied we are. 
+        #If the hunger_level is below 50, we are hungry. 
+        self.hunger_level = 70
+
+    def concider_deactivation(self):
+        if self.hunger_level >= 50:
+            self.active_flag = False
+            return True
+        self.active_flag = True
+        return False
+    
+    def concider_activation(self):
+        if self.hunger_level < 50:
+            self.active_flag = True
+            return True
+        self.active_flag = False
+        return False
+
+    def sense_and_act(self):
+        #This is where we would check if the behavior is hungry, and look for the food source
+        #we could say 'self.match_degree = self.hunger_level // 10, for example, and then request 
+        #that we go to source. Loads of possibilities here, but since we havent got any working sensormodules yet, Ill let it be. 
+        pass
+    def get_update(self):
+        #reduce the hunger_level every time the Behavior is asked for an update to simulate hunger.
+        hunger_level -= 0.001
+        super(self).get_update()
+    
+
