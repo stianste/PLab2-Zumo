1886665a2624aba351cd1b341f207d0a4833284e
Mikkel <mikkel.svagard@gmail.com>
2015-10-19 16:06:34 +0200

    var feil i koden

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from zumo_button import ZumoButton
 def Start():
 	ZumoButton().wait_for_press();
 	m = Motors();
-	int i = 0;
+	i = 0;
 	while(m.button_pressed()):
-		i++;
+		i+=1;
 		m.forward(dur=0.5);
 		if(i>10):
 			m.stop();	
 	m.stop();
 
-Start();	
+Start();
