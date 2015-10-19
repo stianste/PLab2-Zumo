809d7ec95faccfc917d8c580941c1887515ae819
Mikkel <mikkel.svagard@gmail.com>
2015-10-19 16:01:52 +0200

    la til button-pressed

--- zumo_button.py
+++ zumo_button.py
@@ -3,14 +3,17 @@ import wiringpi2 as wp
 
 class ZumoButton():
 
-    def __init__(self):
-        wp.wiringPiSetupGpio()
-        wp.pinMode(22, 0)
-        wp.pullUpDnControl(22, 2)
+	def __init__(self):
+		wp.wiringPiSetupGpio()
+		wp.pinMode(22, 0)
+		wp.pullUpDnControl(22, 2)
 
-    def wait_for_press(self):
-        read_val = wp.digitalRead(22)
-        while read_val:
-            read_val = wp.digitalRead(22)
-        print("Button pressed!!")
+	def wait_for_press(self):
+		read_val = wp.digitalRead(22)
+		while read_val:
+			read_val = wp.digitalRead(22)
+		print("Button pressed!!")
 
+	def button_pressed(self):
+		read_val = wp.digitalRead(22)
+		return (not read_val)
