a5766385f22347aa986a1af5c361743a3fd14fe0
Mikkel <mikkel.svagard@gmail.com>
2015-10-19 17:34:59 +0200

    tester turn away

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from motors import Motors
 from zumo_button import ZumoButton
 from time import sleep
+from test_dogde import Test_dogde
 
 def Start():
 	ZumoButton().wait_for_press();
 	sleep(3);
 	m = Motors();
+	d = Test_dogde();
 	while(not ZumoButton().button_pressed()):
-		m.forward(dur=0.1);
+		speed_value,turn_value = d.get_suggestion();
+		
+		if(turn_value>0):
+			m.left(turn_value);
+		else:
+			m.forward(0.1);
 
 Start();
