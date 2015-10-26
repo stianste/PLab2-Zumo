376ec39760926f22f29ecceb685c11ed28eb0262
stianste <stiansteinbakken17@gmail.com>
2015-10-26 17:31:31 +0100

    Added comperators to motor_rec.py, so the priorityQueue now how to prioritize

--- motor_rec.py
+++ motor_rec.py
@@ -4,4 +4,16 @@ class Motor_Rec():
         self.degrees = degrees
         self.speed = speed
         self.duration = duration
+    def __lt__(self, other):
+        return self.pri < other.pri
+    def __le__(self, other):
+        return self.pri <= other.pri
+    def __eq__(self, other):
+        return self.pri == other.pri
+    def __ne__(self, other):
+        return self.pri != other.pri
+    def __gt__(self, other):
+        return self.pri > other.pri
+    def __ge__(self, other):
+        return self.pri >= other.pri
 
