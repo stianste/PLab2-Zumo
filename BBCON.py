cb562d20373275a3ae1308de1e3604f6f0ff1037
stianste <stiansteinbakken17@gmail.com>
2015-10-26 17:07:21 +0100

    Added Arbitrator class and motor_rec. Also adapted changes to BBCON that Alf made

--- BBCON.py
+++ BBCON.py
@@ -10,21 +10,6 @@ class BBCON():
         self.arbitrator = arbitrator()
         self.motob = motob
 
-    def activate_behavior(self, behavior):
-        #activates behavior if it is in behaviors list, and adds it to the active ones. 
-        if not behavior in self.behaviors:
-            print('Behavior not in behaviors list')
-            return
-        behavior.active_flag = True
-        self.active_behaviors.append(behavior)
-    def deactivate_behavior(self, behavior):
-        #activates behavior if it is in behaviors list, and adds it to the active ones. 
-        if not behavior in self.active_behaviors:
-            print('Behavior not in behaviors list')
-            return
-        behavior.active_flag = False
-        self.active_behaviors.remove(behavior)
-
     def _update_sensobs(self):
           for ob in self.sensobs:
               ob.update()
---
+++ motor_rec.py
@@ -0,0 +1,7 @@
+class Motobs():
+    def __init__(self, pri, degrees, speed, duration):
+        self.pri = pri
+        self.degrees = degrees
+        self.speed = speed
+        self.duration = duration
+
