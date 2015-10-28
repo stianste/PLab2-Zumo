2288ede5a2eff90e3b79421d04400885f58959e7
stianste <stiansteinbakken17@gmail.com>
2015-10-28 14:56:35 +0100

    Added more strict strutcture in Behaviors & Turn
    Added exeptions in functions so we remember to implement them, and
    updatet get_update. Also added TurnAtEdge behavior.

--- Behavior.py
+++ Behavior.py
@@ -4,9 +4,11 @@ class Behavior():
     def __init__(self, sensobs, active_flag, static_priority):
         self.sensobs = sensobs
         self.active_flag = active_flag
-        self.motor_recommendation = [] # List of possible motor recommendations ? Can be implemented differently for each behavior
         # static_priority is a static, final varibale. It should not be changed after init. Dont know how to do this in python
         self.static_priority = static_priority
+        #creates an empty motor rec, this will not be returned unless its been alternated. 
+        self.motor_recommendation = Motor_Rec(0, None)
+        self.match_degree = 0
 
         # Below are some effectivley abstract classes. Each subclass of Behavior have to define these functions themselves. 
     def _update_flag(self):
@@ -17,10 +19,13 @@ class Behavior():
         #1 get an update from all sensobs in its sensob list self.sensob
         #2 make a decision based on the sensor inputs
         #3 update self.motor_recommandations and self.match_degree based on the decision
+        raise Exception('_sense_and_act was not implemented in class Behavior')
         return
 
-
     def get_update(self):
+        #Do not implement this function yourself. Use sense_and_act, and let get update do the rest
+        self._update_flag
         self._sense_and_act()
         self.weight = self.static_priority * self.match_degree
-        return (self.weight, self.motor_request)
+        self.motor_recommendation.pri = weight
+        return (self.motor_recommendation)
--- turnAtEdge.py
+++ turnAtEdge.py
@@ -2,16 +2,27 @@ from Behavior import Behavior
 from motor_rec import Motor_Rec
 from reflectance_sensors import *
 
-class TurnAtEdge():
-	def __init__(self, sensobs, static_pri):
-		super().__init__(sensobs, True, static_pri)
-	
-	def get_update(self):
-    speed_value = 0.25;
-    turn_value = 0.0;
-    score=10;
-    values = self.sensobs['ir'].get_value()
-    print("distance: " + str(values));
-
-    
-    return Motor_Rec(10, 0, speed_value, 2)
+class TurnAtEdge(Behavior):
+    threshold = 0.3
+    pri = 10
+    def __init__(self, sensobs):
+            super().__init__(sensobs, True, pri)
+            self.ir = sensobs['ir'].get_value()
+    def _update_flag(self):
+        #Checks the urrent values of the ir sensor and checks if any of them are darker than threshold
+        values = self.ir.get_value
+        for v in values:
+            if v < threshold:
+                self.active_flag = True
+                return True
+        return False
+        
+    def _sense_and_act(self):
+        speed_value = 0.25;
+        turn_value = 0.0;
+        score=10;
+        values = self.sensobs['ir'].get_value()
+        #print("distance: " + str(values));
+        if self.active_flag:
+            self.match_degree = 200
+            self.motor_recommendation.action = ['l', 90] 
