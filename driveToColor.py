4dc483065cbc2a0ca1f4c2cc8600ccf88fb2763c
Alf Alexander <alf1337@gmail.com>
2015-10-28 14:25:03 +0100

    The bot now looks for black instead of blue, and selects the median of the black pixels found to determine where it should drive to

--- driveToColor.py
+++ driveToColor.py
@@ -19,22 +19,20 @@ class driveToColor(Behavior):
       w = img.xmax
       img = img.crop((0,half,w,half+1)) # Img is now a one-pixel tall image, same width
       # The plan is to find the first blue pixel and the last one, then finding the center of this. Lastly, drive towards it
-      first = 0
-      last = w
+      readings = []
       for i in range(w):
         rgb = img.get_pixel(i,0) # If this one is above 100 we can assume it is blue for now
-        if rgb[2] > rgb[0] and rgb[2] > rgb[0] and first == 0:
-          first = i
-        if rgb[2] > rgb[0] and rgb[2] > rgb[0]:
-          last = i
+        if rgb[0] <= 50 and rgb[1] <= 25 and rgb[2] <= 15:
+          readings.append(i)
 
-      center = w // 2
-      center_blue = (first+last)//2
+      center = w / 2
+      center_black = readings[(len(readings)+1)//2]
 
-      if center_blue > center:
-        return (((center_blue-center)/center) * 26.75)
+      if center_black > center:
+        return (round(((center_black-center)/center) * 26.75))
       else: # center_blue < center
-        return (-((center-center_blue)/center) * 26.75)
+        return (round(-((center-center_black)/center) * 26.75))
+
 
     def _sense_and_act(self):
         img = Imager(image=self.sensobs['camera'].get_value(), mode='RGB') # Get the image from the camera sensob, the value should be updated from the bbcon
