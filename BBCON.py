f2d79f312c8714f75599de375e6608afd37a9f45
Mikkel <mikkel.svagard@gmail.com>
2015-10-28 14:50:04 +0100

    endret til m.action

--- BBCON.py
+++ BBCON.py
@@ -31,7 +31,7 @@ class BBCON():
       self._update_sensobs()
       self._update_behaviors()
       mr = self.arbitrator.choose_action()
-      self.motors.do(mr)
+      self.motors.do(mr.action)
       sleep(0.01)
 
 
