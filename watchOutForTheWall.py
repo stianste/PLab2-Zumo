574a55a8b66d7eb4c207683d5c0f48d995610e98
Mikkel <mikkel.svagard@gmail.com>
2015-10-28 14:21:48 +0100

    tweaket action-verdiene

--- watchOutForTheWall.py
+++ watchOutForTheWall.py
@@ -6,20 +6,20 @@ from ultrasonic import *
 class WatchOutForTheWall(Behavior):
   def __init__(self, sensobs, static_pri):
     super().__init__(sensobs, True, static_pri)
-
+	
+	
   def get_update(self):
-    speed_value = 0.25;
-    turn_value = 0.0;
+    action = 0.25;
     score=10;
-    distance = self.sensobs['ultrasonic'].get_value()
+    values = self.sensobs['ultrasonic'].get_value()
     print("distance: " + str(distance));
 
     if(distance<10 and distance>0):
-      speed_value *= -1
+		action = ["r",45];
     elif distance<15 and distance>0:
-      speed_value = 0;
+      action = 0;
     elif(distance<40 and distance>0):
-      speed_value *= 1
+      action = (distance/60)
     else:
-      speed_value *= 2
-    return Motor_Rec(10, 0, speed_value, 2)
+      action = 1;
+    return Motor_Rec(10, action)
