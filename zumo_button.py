9d722eb699e7883ba90edb76d3462cb6c98f405d
Mikkel <mikkel.svagard@gmail.com>
2015-10-28 17:20:50 +0100

    lhaha

--- zumo_button.py
+++ zumo_button.py
@@ -18,6 +18,6 @@ class ZumoButton():
     read_val = wp.digitalRead(22)
     print(read_val);
     if(read_val==0):
-    print("pressed again")
+      print("pressed again")
       return False;
     return True;
